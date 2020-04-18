de38e91206a70f86a07e9faa1235b6459f840ba8
Dg-byte <dgolikov40@gmail.com>
2020-04-18 19:25:03 +0700

    my message

---
+++ script.js
@@ -0,0 +1,16 @@
+$("#prev").click(function(e){
+    $("#slideShow").carousel('prev');
+});
+$("#next").click(function(e){
+    $("#slideShow").carousel('next');
+});
+$('#play').click(function() {
+    let controls = $(this);
+    if (controls.hasClass('pause')) {
+        controls.text('▶').removeClass('pause').toggleClass('play');
+        $('#slideShow').carousel('pause');
+    } else {
+        controls.text('⏸').removeClass('play').toggleClass('pause');
+        $('#slideShow').carousel('cycle');
+    }
+});
